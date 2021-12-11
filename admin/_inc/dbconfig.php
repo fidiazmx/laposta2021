bec4de1515d76472e0f4cfa2f0bd487addc0cd2e
Fidel Diaz <fideldiaz@MacBook-Air-de-Fidel.local>
2021-12-11 08:21:23 -0600

    datos conexion remota

--- admin/_inc/dbconfig.php
+++ admin/_inc/dbconfig.php
@@ -1,10 +1,12 @@
 <?php 
 
+//mysql://b2ca8ba5936d96:4c3fc5e5@us-cdbr-east-05.cleardb.net/heroku_7c1482fd2536ff5?reconnect=true
+
 if ($_SERVER['HTTP_HOST'] == 'laposta2021.herokuapp.com') {
-    $db_server   = 'us-cdbr-east-04.cleardb.com';
-    $db_username = 'b187311181ea3a';
-    $db_password = '4de3c0bc';
-    $db_name     = 'heroku_f6286574aa75e0d';
+    $db_server   = 'us-cdbr-east-05.cleardb.net';
+    $db_username = 'b2ca8ba5936d96';
+    $db_password = '4c3fc5e5';
+    $db_name     = 'heroku_7c1482fd2536ff5';
 } else if ($_SERVER['HTTP_HOST'] == 'laposta.local')  {
     $db_server   = 'localhost';
     $db_username = 'admin';
