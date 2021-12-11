547ce19f6999e60b23074e66855f5ddde2b582b9
Fidel Diaz <fideldiaz@MacBook-Air-de-Fidel.local>
2021-12-11 08:55:22 -0600

    remoto update

--- admin/app-assets/js/urlbase.js
+++ admin/app-assets/js/urlbase.js
@@ -7,7 +7,7 @@ const urlbasedevlocal = "laposta.local/admin";
 
 //var usourl = window.location.hostname == urlbase  ? urlbase : urldev;
 var usourl;
-usourl = 'https://' + urlbaseprod
+usourl = 'http://' + urlbaseprod
 /*
 switch (window.location.hostname) {
     case urlbaseprod:
